862454196d34b6bfa4aa7e5612d3a27b6bfc0c94
celinechole <github@celinechole.com>
2019-01-14 13:07:32 -0500

    fixes pwd

--- bash.js
+++ bash.js
@@ -1,19 +1,10 @@
-// process.stdout.write('prompt > ');
-
-// process.stdin.on('data', (data) => {
-//     const cmd = data.toString().trim();
-
-//     process.stdout.write('You typed: ' + cmd);
-//     process.stdout.write('\nprompt > ');
-
-// });
-
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', () => {
-    const cmd = String(process.cwd());
+process.stdin.on('data', (data) => {
+    const cmd = data.toString().trim();
 
-    process.stdout.write(console.log(`${cmd}`));
+    process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
 
 });
+
