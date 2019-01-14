1021c2c3a0cd9fe059ce39222613b81d1bdd8fce
Cassie Rosner <cerosner@gmail.com>
2019-01-14 12:50:58 -0500

    attempt pwd command

--- bash.js
+++ bash.js
@@ -1,9 +1,19 @@
+// process.stdout.write('prompt > ');
+
+// process.stdin.on('data', (data) => {
+//     const cmd = data.toString().trim();
+
+//     process.stdout.write('You typed: ' + cmd);
+//     process.stdout.write('\nprompt > ');
+
+// });
+
 process.stdout.write('prompt > ');
 
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
+process.stdin.on('data', () => {
+    const cmd = String(process.cwd());
 
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write(console.log(`${cmd}`));
     process.stdout.write('\nprompt > ');
 
 });
